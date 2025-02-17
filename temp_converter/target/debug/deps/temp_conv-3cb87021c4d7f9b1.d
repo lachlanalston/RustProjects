f210268b17f7cc088db99008c0706a9e904a4aa2
lachlanalston <41813866+lachlanalston@users.noreply.github.com>
2025-02-17 12:36:11 +1100

    Delete temp_converter/target directory

--- temp_converter/target/debug/deps/temp_conv-3cb87021c4d7f9b1.d
+++
@@ -1,5 +0,0 @@
-/home/monarch/Documents/RustProjects/temp_converter/target/debug/deps/libtemp_conv-3cb87021c4d7f9b1.rmeta: src/main.rs
-
-/home/monarch/Documents/RustProjects/temp_converter/target/debug/deps/temp_conv-3cb87021c4d7f9b1.d: src/main.rs
-
-src/main.rs:
--- temp_converter/target/debug/temp_conv.d
+++
@@ -1,1 +0,0 @@
-/home/monarch/Documents/rust-lib/temp_conv/target/debug/temp_conv: /home/monarch/Documents/rust-lib/temp_conv/src/main.rs
