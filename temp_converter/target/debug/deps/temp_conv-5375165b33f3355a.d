d36d762933a44ccf6cc3bd727b66e6cc048f73e3
Lachlan Alston <lachlan@alston.id.au>
2025-02-17 12:36:48 +1100

    Added

---
+++ temp_converter/target/debug/deps/temp_conv-5375165b33f3355a.d
@@ -0,0 +1,5 @@
+/home/monarch/Documents/RustProjects/temp_converter/target/debug/deps/libtemp_conv-5375165b33f3355a.rmeta: src/main.rs
+
+/home/monarch/Documents/RustProjects/temp_converter/target/debug/deps/temp_conv-5375165b33f3355a.d: src/main.rs
+
+src/main.rs:
---
+++ temp_converter/target/debug/deps/temp_conv-c4d8533404ff757e.d
@@ -0,0 +1,5 @@
+/home/monarch/Documents/RustProjects/temp_converter/target/debug/deps/libtemp_conv-c4d8533404ff757e.rmeta: src/main.rs
+
+/home/monarch/Documents/RustProjects/temp_converter/target/debug/deps/temp_conv-c4d8533404ff757e.d: src/main.rs
+
+src/main.rs:
