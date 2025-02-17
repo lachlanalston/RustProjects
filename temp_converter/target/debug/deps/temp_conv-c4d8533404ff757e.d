501ec0c35fa42ba610cc9fb3bd2c580dafa531d5
Lachlan Alston <lachlan@alston.id.au>
2025-02-17 12:46:12 +1100

    Stopped tracking of temp_converter/target

--- temp_converter/target/debug/deps/temp_conv-c4d8533404ff757e.d
+++
@@ -1,5 +0,0 @@
-/home/monarch/Documents/RustProjects/temp_converter/target/debug/deps/libtemp_conv-c4d8533404ff757e.rmeta: src/main.rs
-
-/home/monarch/Documents/RustProjects/temp_converter/target/debug/deps/temp_conv-c4d8533404ff757e.d: src/main.rs
-
-src/main.rs:
