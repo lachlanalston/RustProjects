2e9f680723d19acf8eae80ed4c4764211dd14249
Lachlan Alston <lachlan@alston.id.au>
2025-03-01 11:31:56 +1100

    remove the-book/target from repo

--- the_book/target/debug/deps/the_book-30643fe4326e0ddf.d
+++
@@ -1,5 +0,0 @@
-/home/monarch/Documents/RustProjects/the_book/target/debug/deps/the_book-30643fe4326e0ddf: src/main.rs
-
-/home/monarch/Documents/RustProjects/the_book/target/debug/deps/the_book-30643fe4326e0ddf.d: src/main.rs
-
-src/main.rs:
--- the_book/target/debug/the_book.d
+++
@@ -1,1 +0,0 @@
-/home/monarch/Documents/RustProjects/the_book/target/debug/the_book: /home/monarch/Documents/RustProjects/the_book/src/main.rs
