35bd9b5df886d15532ae88cfc8ec1e7005ee78e1
Lachlan Alston <lachlan@alston.id.au>
2025-03-01 11:25:26 +1100

    Added Folders for rustlings & the book

---
+++ the_book/target/debug/deps/the_book-30643fe4326e0ddf.d
@@ -0,0 +1,5 @@
+/home/monarch/Documents/RustProjects/the_book/target/debug/deps/the_book-30643fe4326e0ddf: src/main.rs
+
+/home/monarch/Documents/RustProjects/the_book/target/debug/deps/the_book-30643fe4326e0ddf.d: src/main.rs
+
+src/main.rs:
---
+++ the_book/target/debug/the_book.d
@@ -0,0 +1,1 @@
+/home/monarch/Documents/RustProjects/the_book/target/debug/the_book: /home/monarch/Documents/RustProjects/the_book/src/main.rs
